54c86057e5a486c0b0ae2e78430d4204e2d405c0
kgyuvraj <52133812+kgyuvraj@users.noreply.github.com>
2022-07-25 19:49:51 +0530

    Update main.groovy

--- main.groovy
+++ main.groovy
@@ -3,3 +3,12 @@ job('example') {
     shell('echo Hello World!')
   }
 }
+
+@Grab('org.yaml:snakeyaml:1.17')
+
+import org.yaml.snakeyaml.Yaml
+
+Yaml parser = new Yaml()
+List example = parser.load(("pipelines/a.yaml" as File).text)
+
+example.each{println it.subject}
