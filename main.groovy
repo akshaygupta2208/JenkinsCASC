fe3cbd8194df2cc210a2f95dfa69feba1b51c5f5
kgyuvraj <52133812+kgyuvraj@users.noreply.github.com>
2022-07-26 19:20:07 +0530

    Update main.groovy

--- main.groovy
+++ main.groovy
@@ -3,14 +3,14 @@ job('example') {
     shell('echo Hello World!')
   }
 }
-'''
+
 @Grab('org.yaml:snakeyaml:1.17')
 
 import org.yaml.snakeyaml.Yaml
 def current_workspace = getBinding().getVariables()['WORKSPACE']
-
+println(current_workspace)
 import groovy.io.FileType
-
+'''
 def list = []
 
 def dir = new File(current_workspace+"/pipelines")
