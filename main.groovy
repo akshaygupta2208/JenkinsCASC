86d5d13debe23ef1321f33441f8cc0e730fe41ea
kgyuvraj <52133812+kgyuvraj@users.noreply.github.com>
2022-07-27 19:14:01 +0530

    Update main.groovy

--- main.groovy
+++ main.groovy
@@ -33,7 +33,7 @@ list.each {
   def v = example["name"]
   println(v.getClass())
   //println(example["name"])
-  pipelineJob(v) {
+  pipelineJob("a") {
     definition {
         cpsScm {
             scm {
