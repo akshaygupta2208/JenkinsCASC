6b5fc80eb7fc8496a152ed27642f1fc4c4261380
kgyuvraj <52133812+kgyuvraj@users.noreply.github.com>
2022-07-28 14:51:58 +0530

    Update main.groovy

--- main.groovy
+++ main.groovy
@@ -49,7 +49,7 @@ pipelineJob(example["name"]) {
                             sh 'echo $v'
                             git branch: 'master',
                             credentialsId: 'kgyuvraj',
-                            url: System.getenv("some_var")
+                            url: 'System.getenv("some_var")'
                             }    
                     }
                     stage('Build') {     
