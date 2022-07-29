e2ff1b10959c425a69d8b8fc68cda25f014d42e4
kgyuvraj <52133812+kgyuvraj@users.noreply.github.com>
2022-07-29 13:39:16 +0530

    Update main.groovy

--- main.groovy
+++ main.groovy
@@ -1,8 +1,9 @@
-job('example') {
-  steps {
-    shell('echo Hello World!')
-  }
-}
+// Template basic job
+// job('example') {
+//   steps {
+//     shell('echo Hello World!')
+//   }
+// }
 
 @Grab('org.yaml:snakeyaml:1.17')
 
@@ -74,30 +75,8 @@ stage('Deployqa') {
                                 }    
                         }
 """
-dev_stage = ""
-def dir = new File(current_workspace+"/pipelines")
-dir.eachFileRecurse (FileType.FILES) { file ->
-  list << file
-}
-
-list.each {
-  println it.path
-  
-  Yaml parser = new Yaml()
-  example = parser.load((it.path as File).text)
-  repourl = example["repoUrl"]
-  deployenv = example["deployEnv"]
-  println("this is deploy env "+deployenv)
-  println(example["deployEnv"])
-  
-pipelineJob(example["name"]) {
-    definition {
-        cps {
-            script("""
-            pipeline {
-                agent any
-                stages {
-                    stage('Checkout Stage') {     
+basic_stage = """
+stage('Checkout Stage') {     
                         steps{  
                             sh 'echo "Checkout"'
                             git branch: 'master',
@@ -120,7 +99,36 @@ pipelineJob(example["name"]) {
                                 sh 'echo "ArtefactCreation"'
                                 }    
                         }
+"""
+def dir = new File(current_workspace+"/pipelines")
+dir.eachFileRecurse (FileType.FILES) { file ->
+  list << file
+}
+
+list.each {
+  println it.path
+  
+  Yaml parser = new Yaml()
+  example = parser.load((it.path as File).text)
+  repourl = example["repoUrl"]
+  deployenv = example["deployEnv"]
+  println("this is deploy env "+deployenv)
+  println(example["deployEnv"])
+  
+pipelineJob(example["name"]) {
+    definition {
+        cps {
+            script("""
+            pipeline {
+                agent any
+                stages {
+                    ${basic_stage}
+                    if(deployenv.contains('dev')){
                     ${dev_stage}
+                    }
+                    else{
+                    dev_stage = ""}
+                    
                     ${prod_stage}
                     ${stg_stage}
                     ${qa_stage}
