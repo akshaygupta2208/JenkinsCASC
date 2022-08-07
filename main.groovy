fa35408fba4b65d394a93af5306f799fa0437341
kgyuvraj <52133812+kgyuvraj@users.noreply.github.com>
2022-08-07 18:47:39 +0530

    Update main.groovy

--- main.groovy
+++ main.groovy
@@ -134,11 +134,11 @@ stage('Deployqa') {
                                 }    
                         }
                     stage('Docker Build') {
-      steps {
-        sh 'ls -l'
-        sh 'docker build -t petclinic:latest .'
-      }
-    }
+                              steps {
+                                sh 'ls -l'
+                                sh 'docker build -t petclinic:latest .'
+                              }
+                            }
                     stage('BuildSanity') {     
                             steps{  
                                 sh 'echo "BuildSanity"'
@@ -147,10 +147,7 @@ stage('Deployqa') {
                     stage('ArtefactCreation') {     
                             steps{  
                                 sh 'echo "ArtefactCreation"'
-        //   sh "docker login https://nexus.softwaremathematics.com/"
-        //   sh "docker build -t nexus.softwaremathematics.com/petclinic ."
-        //   sh 'docker push nexus.softwaremathematics.com/petclinic'
-        withCredentials([usernamePassword(credentialsId: 'nexus', passwordVariable: 'kgb', usernameVariable: 'admin')]) {
+          withCredentials([usernamePassword(credentialsId: 'nexus', passwordVariable: 'kgb', usernameVariable: 'admin')]) {
           sh "docker login -u ${env.admin} -p ${env.kgb} https://nexus.softwaremathematics.com/"
           sh "docker build -t nexus.softwaremathematics.com/petclinic ."
           sh "docker push nexus.softwaremathematics.com/petclinic"
