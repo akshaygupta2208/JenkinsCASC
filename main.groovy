1a4ca11a79c7e095e4b379078068c2f16068e384
kgyuvraj <52133812+kgyuvraj@users.noreply.github.com>
2022-08-08 17:27:08 +0530

    Update main.groovy

--- main.groovy
+++ main.groovy
@@ -143,7 +143,7 @@ stage('Deployqa') {
                             steps{  
                                 sh 'echo "ArtefactCreation"'
                                 withCredentials([usernamePassword(credentialsId: 'nexus', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-                                sh "echo System.getenv(USERNAME)"
+                                //sh "echo System.getenv(USERNAME)"
                                 sh "docker login -u admin -p kgb https://nexus.softwaremathematics.com/"
                                 sh "docker build -t nexus.softwaremathematics.com/petclinic:latest ."
                                 sh "docker push nexus.softwaremathematics.com/petclinic:latest"
