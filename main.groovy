5438e667fc34d1e5aa6d6f454e6de7121b9382a5
kgyuvraj <52133812+kgyuvraj@users.noreply.github.com>
2022-09-08 10:27:22 +0530

    Update main.groovy

--- main.groovy
+++ main.groovy
@@ -181,13 +181,16 @@ list.each {
                 
                     stage('Build') {     
                             steps{
-                                dir("null"){
-                                    configFileProvider(
-                                        [configFile(fileId: 'mvn-settings', variable: 'MAVEN_SETTINGS')]) {
-                                            sh 'echo "Build"'
-                                            sh 'echo hello world -s $MAVEN_SETTINGS'                                
-                                        }
-                                }
+//                                 dir("null"){
+//                                     configFileProvider(
+//                                         [configFile(fileId: 'mvn-settings', variable: 'MAVEN_SETTINGS')]) {
+//                                             sh 'echo "Build"'
+//                                             sh 'echo hello world -s $MAVEN_SETTINGS'                                
+//                                         }
+//                                 }
+                                               sh 'echo "Build"'
+                                               sh 'echo "hello world"'
+                                                
                             }    
                     }
                     stage('BuildSanity') {     
