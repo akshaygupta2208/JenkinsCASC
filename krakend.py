4b689ebe78a0056f504a914bf9c85b232c082571
akshay.gupta <akshay@rupeek.com>
2022-09-29 22:55:27 +0530

    krakend dynamic json added

---
+++ krakend.py
@@ -0,0 +1,79 @@
+# python jenkins/krakend.py
+import json
+import os
+import requests
+import yaml
+from glob import glob
+
+
+krakend_base_json = {
+    "version": 3,
+    "endpoints": []
+}
+pipeline_base = "jenkins/pipelines"
+krakend_base_json_path = "ansible/roles/apithfrole/files"
+
+
+def get_recursive_files(base_path):
+    result = [y for x in os.walk(base_path) for y in glob(os.path.join(x[0], '*.y*ml'))]
+    return result
+
+
+def read_yaml(yaml_file):
+    """ A function to read YAML file"""
+    with open(yaml_file) as f:
+        config = list(yaml.safe_load_all(f))
+    return config
+
+
+def get_swagger_data(app_base, swagger_uri="/v2/api-docs"):
+    # swagger uri = /v2/api-docs
+    print(f'Looking for swagger data for {app_base}{swagger_uri}')
+    try:
+        response = requests.get(f'http://{app_base}{swagger_uri}')
+        print(f'Swagger data api returned {response.status_code}')
+        if response.status_code == 200:
+            return response.json()
+    except requests.exceptions.ConnectionError as e:
+        print("Swagger data api is not responding")
+    return False
+
+for pipeline_file in get_recursive_files(pipeline_base):
+    print(f'Working on file {pipeline_file}')
+
+    # get all necessary details
+    yaml_config = read_yaml(pipeline_file)
+    # deploy_port
+    if "deploy_port" in yaml_config[0] and "deploy_servers" in yaml_config[0]:
+        deploy_port = yaml_config[0].get("deploy_port")
+        app_name = yaml_config[0].get("name")
+        deploy_servers = yaml_config[0].get("deploy_servers")
+        print("Application has deploy_port looking for swagger config now")
+        # get swagger data if present
+        swagger_data = get_swagger_data(f'{deploy_servers[0]}:{deploy_port}')
+        if swagger_data:
+            if "paths" in swagger_data:
+                for path in swagger_data["paths"]:
+                    # generating krakend config here
+                    krakend_config = {}
+                    krakend_config["endpoint"] = f"{app_name}{path}"
+                    krakend_config["output_encoding"] = "no-op"
+                    krakend_config["backend"] = []
+                    hosts = []
+                    for server in deploy_servers:
+                        hosts.append(f"{server}:{deploy_port}")
+                    backend = {
+                        "encoding": "no-op",
+                        "url_pattern": path,
+                        "host": hosts
+                    }
+                    krakend_config["backend"].append(backend)
+                    krakend_base_json["endpoints"].append(krakend_config)
+
+# Serializing json
+json_object = json.dumps(krakend_base_json, indent=4)
+print(json.dumps(json_object, indent=4))
+
+# Writing to sample.json
+with open(f"{krakend_base_json_path}/krakend.json", "w") as outfile:
+    outfile.write(json.dumps(krakend_base_json, indent=4))
