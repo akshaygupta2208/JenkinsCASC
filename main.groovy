b452dd9b4063fbf3ca07c08f7b04415a870c69b7
kgyuvraj <52133812+kgyuvraj@users.noreply.github.com>
2022-09-30 11:53:12 +0530

    Update main.groovy

--- main.groovy
+++ main.groovy
@@ -46,12 +46,12 @@ list.each {
                                 dir(\"app/${src_path}\"){ 
                                 sh "docker login -u \${NEXUS_CRED_USR} -p \${NEXUS_CRED_PSW} ${NEXUS_REPO_URL}"
                                 
-                                sh "VERSION=$(date %Y%m%d%H%M%S).git.$GIT_REVISION
-                                sh "IMAGE=${NEXUS_DOCKER_REPO_BASE}/${name}"
-                                sh "docker --network=host build -t $IMAGE:$VERSION ."
-                                sh "docker push $IMAGE/$VERSION"
-//                                 sh "docker build --network=host -t ${NEXUS_DOCKER_REPO_BASE}/${name}:latest ."
-//                                 sh "docker push ${NEXUS_DOCKER_REPO_BASE}/${name}:latest"
+//                                 sh "VERSION=$(date %Y%m%d%H%M%S).git.$GIT_REVISION
+//                                 sh "IMAGE=${NEXUS_DOCKER_REPO_BASE}/${name}"
+//                                 sh "docker --network=host build -t $IMAGE:$VERSION ."
+//                                 sh "docker push $IMAGE/$VERSION"
+                                 sh "docker build --network=host -t ${NEXUS_DOCKER_REPO_BASE}/${name}:latest ."
+                                 sh "docker push ${NEXUS_DOCKER_REPO_BASE}/${name}:latest"
                                  }
                     }
                     }
