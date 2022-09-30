a82aa5298e133f62712ec02c64928b41bb6b1926
kgyuvraj <52133812+kgyuvraj@users.noreply.github.com>
2022-09-30 13:51:42 +0530

    Update main.groovy

--- main.groovy
+++ main.groovy
@@ -45,8 +45,8 @@ list.each {
                                 sh 'echo "ArtefactCreation"'
                                 dir(\"app/${src_path}\"){ 
                                 sh "docker login -u \${NEXUS_CRED_USR} -p \${NEXUS_CRED_PSW} ${NEXUS_REPO_URL}"
-                                 sh "docker build --network=host -t ${NEXUS_DOCKER_REPO_BASE}/${name}:latest ."
-                                 sh "docker push ${NEXUS_DOCKER_REPO_BASE}/${name}:latest"
+                                 sh "docker build --network=host -t ${NEXUS_DOCKER_REPO_BASE}/${name}:${BUILD_TIMESTAMP} ."
+                                 sh "docker push ${NEXUS_DOCKER_REPO_BASE}/${name}:${BUILD_TIMESTAMP}"
                                  }
                     }
                     }
