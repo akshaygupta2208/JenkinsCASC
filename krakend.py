836ad7ee44c2f2a652b7632eed6bdb7ce34380ab
akshay.gupta <akshay@rupeek.com>
2022-10-11 23:03:42 +0530

    krakend dynamic json added

--- krakend.py
+++ krakend.py
@@ -118,6 +118,7 @@ for pipeline_file in get_recursive_files(pipeline_base):
                         method = method.upper()
                         krakend_config = {}
                         krakend_config["endpoint"] = f"/{app_name}{path}"
+                        krakend_config["input_query_strings"] = ["*"]
                         krakend_config["output_encoding"] = "json"
                         krakend_config["input_headers"] = ["Content-Type"]
                         krakend_config["method"] = method
