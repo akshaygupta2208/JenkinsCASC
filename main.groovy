b46c142ec76ee3525b5199f18a9f695b6d08d9ef
kgyuvraj <kgyuvraj@gmail.com>
2022-10-13 09:33:19 +0530

    updated

--- main.groovy
+++ main.groovy
@@ -465,11 +465,12 @@ pipelineJob('Infra/monitoring-server'){
 }
 pipelineJob('Infra/create-user'){
 
+    parameters {
+        stringParam(parameterName: "USERNAME", defaultValue: "root", trim: true, description: "Sample string parameter")
+        stringParam(parameterName:'Password', defaultValue:null, 'Enter the password of the remote host')
+    }
     definition {
-        parameters {
-            string(name: "USERNAME", defaultValue: "root", trim: true, description: "Sample string parameter")
-            stringParam('Password', 'Blank', 'Enter the password of the remote host')
-        }
+
         cps {
             script("""
 
