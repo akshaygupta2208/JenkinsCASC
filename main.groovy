a5cac5f6848daba8e682331bdbe27558068b2527
kgyuvraj <kgyuvraj@gmail.com>
2022-10-13 09:23:51 +0530

    updated

--- main.groovy
+++ main.groovy
@@ -509,14 +509,13 @@ parameters {
                     stage('BuildSanity') {     
                             steps{  
                                 sh 'echo "BuildSanity"'
-                                sh 'echo "params.USERNAME"'
                               }    
                     }  
                     stage("execute Ansible") {
            steps {
                
                 withEnv(["CONTAINER_NAME=department-service","CONTAINER_IMAGE=nexus.softwaremathematics.com/department-service", "deploy_port=9085", "application_port=9000"]) {
-                ansiblePlaybook credentialsId: 'private-key', disableHostKeyChecking: true, installation: 'Ansible', playbook: 'ansible/createuser.yml', extras: '--extra-vars "ansible_user=root ansible_password=Smathematics" -i "38.242.198.101,"'
+                ansiblePlaybook credentialsId: 'private-key', disableHostKeyChecking: true, installation: 'Ansible', playbook: 'ansible/createuser.yml', extras: '--extra-vars "ansible_user=${USERNAME} ansible_password=Smathematics" -i "38.242.198.101,"'
             
                
                }    
