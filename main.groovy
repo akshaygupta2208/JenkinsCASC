39c7e784d9de0bdbf8d1ecd588e4da00ddb4a10c
kgyuvraj <kgyuvraj@gmail.com>
2022-10-28 09:55:01 +0530

    updated

--- main.groovy
+++ main.groovy
@@ -42,9 +42,16 @@ list.each {
     dev_deploy = ""
     
     
-    if (example["deploy_servers"]!= null) {
-        for (server in example["deploy_servers"]) {
+    if (example["deploy_servers_dev"]!= null) {
+        for (server in example["deploy_servers_dev"]) {
             dev_deploy = dev_deploy + """withEnv(["CONTAINER_NAME=${name}","CONTAINER_IMAGE=${NEXUS_DOCKER_REPO_BASE}/${name}", "deploy_port=${deploy_port}", "application_port=${application_port}"]) {
+                ansiblePlaybook credentialsId: 'private-key', disableHostKeyChecking: true, installation: 'Ansible', playbook: 'ansible/deployapp.yml', extras: \'-i \"${server},\"\'
+            """
+        }
+    }
+    if (example["deploy_servers_prod"]!= null) {
+        for (server in example["deploy_servers_prod"]) {
+            prod_deploy = prod_deploy + """withEnv(["CONTAINER_NAME=${name}","CONTAINER_IMAGE=${NEXUS_DOCKER_REPO_BASE}/${name}", "deploy_port=${deploy_port}", "application_port=${application_port}"]) {
                 ansiblePlaybook credentialsId: 'private-key', disableHostKeyChecking: true, installation: 'Ansible', playbook: 'ansible/deployapp.yml', extras: \'-i \"${server},\"\'
             """
         }
@@ -88,6 +95,7 @@ list.each {
                     stage('DeployProd') {
                             steps{  
                                 sh 'echo "DeployProd"'
+                                ${prod_deploy}
                                 }    
                         }
                     stage('ProdSanity') {
