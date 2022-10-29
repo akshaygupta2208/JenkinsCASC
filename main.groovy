0e25f1b917905afdd216738d009aaf39f4ab49a5
kgyuvraj <kgyuvraj@gmail.com>
2022-10-29 23:01:33 +0530

    updated

--- main.groovy
+++ main.groovy
@@ -63,12 +63,13 @@ list.each {
     }
     if (example["deploy_env_variable"] != null) {
         for (var in example["deploy_env_variable"]) {
-            for (i in var){
-            deploy_envir = deploy_envir + """withEnv(["CONTAINER_NAME=${name}","CONTAINER_IMAGE=${NEXUS_DOCKER_REPO_BASE}/${name}", "deploy_port=${deploy_port}", "application_port=${application_port}"]) {
+            for (i in var) {
+                deploy_envir = deploy_envir + """withEnv(["CONTAINER_NAME=${name}","CONTAINER_IMAGE=${NEXUS_DOCKER_REPO_BASE}/${name}", "deploy_port=${deploy_port}", "application_port=${application_port}"]) {
                 ansiblePlaybook credentialsId: 'private-key', disableHostKeyChecking: true, installation: 'Ansible', playbook: 'ansible/deployapp.yml', extras: \'-i \"${i},\"\'
-                 }         
+                         
                  }   
             """
+            }
         }
     }
 
