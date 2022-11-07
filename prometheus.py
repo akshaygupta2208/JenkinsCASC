7b091a9b69da91a42bdc4150ed173ddf2bf5657f
kgyuvraj <kgyuvraj@gmail.com>
2022-11-07 09:11:44 +0530

    update

--- prometheus.py
+++ prometheus.py
@@ -11,7 +11,7 @@ dev_item_list = []
 infra_endpoints = {
     "Grafana": "https://grafana.softwaremathematics.com",
     "Jenkins": "https://jenkins.softwaremathematics.com/login?from=%2F",
-    "Apithf" : "https://apithf.softwaremathematics.com",
+    "Apithf" : "https://apithf.softwaremathematics.com/__health",
     "Nexus":  "https://nexus.softwaremathematics.com"
 }
 pipeline_base = "jenkins/pipelines"
