862eb4dfba5f9037a6ab6cc457d91f1dac49cc6c
kgyuvraj <kgyuvraj@gmail.com>
2022-11-17 17:23:17 +0530

    updated

--- main.groovy
+++ main.groovy
@@ -61,17 +61,17 @@ list.each {
             """
         }
     }
-//    if (example["deploy_env_variable"] != null) {
-//        for (var in example["deploy_env_variable"]) {
-//            for (i in var) {
-//                deploy_envir = deploy_envir + """withEnv(["CONTAINER_NAME=${name}","CONTAINER_IMAGE=${NEXUS_DOCKER_REPO_BASE}/${name}", "deploy_port=${deploy_port}", "application_port=${application_port}", "SM_ENV=${example["deploy_env_variable"]}"]) {
-//                ansiblePlaybook credentialsId: 'private-key', disableHostKeyChecking: true, installation: 'Ansible', playbook: 'ansible/deployapp.yml', extras: \'-i \"${server},\"\'
-//
-//                 }
-//            """
-//            }
-//      }
-//    }
+    if (example["deploy_env_variable"] != null) {
+        for (var in example["deploy_env_variable"]) {
+            for (i in var) {
+                deploy_envir = deploy_envir + """withEnv(["CONTAINER_NAME=${name}","CONTAINER_IMAGE=${NEXUS_DOCKER_REPO_BASE}/${name}", "deploy_port=${deploy_port}", "application_port=${application_port}", "SM_ENV=${example["deploy_env_variable"]}"]) {
+                ansiblePlaybook credentialsId: 'private-key', disableHostKeyChecking: true, installation: 'Ansible', playbook: 'ansible/deployapp.yml', extras: \'-i \"${server},\"\'
+
+                 }
+            """
+            }
+      }
+    }
 
     artefact_creation = """
                     stage('ArtefactCreation') { 
