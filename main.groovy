a46eb1783d3ab9baa731f929b0532f4cc01cb32c
kgyuvraj <kgyuvraj@gmail.com>
2022-11-30 12:36:36 +0530

    main.groovy testing of round off issue

--- main.groovy
+++ main.groovy
@@ -52,10 +52,9 @@ list.each {
             int c = b[0].toInteger()
             println(c)
             max_memory = c*2
-            max_memory = ((max_memory + 99) / 100)
-            max_memory = max_memory -1
-            max_memory = max_memory.round(0)
-            max_memory = max_memory * 100
+            max_memory = max_memory/100
+            max_memory = round(max_memory)
+
 
             println(max_memory)
         }
